de39aa89a4a9dfdd0f621a895a8fffcb8547270e
jesusrda <jrdadb@gmail.com>
2019-10-06 18:12:15 -0400

    Cleanning main

--- Main.hs
+++ Main.hs
@@ -2,10 +2,7 @@ import Lexer
 
 main = do
     f <- getLine
-    putStrLn $ f ++ ":"
     s <- readFile f
-    putStrLn s
-    putStrLn ""
     case scanner s of
         Left s -> putStr s
         Right toks -> putStr $ unlines $ map showTokenPos toks
